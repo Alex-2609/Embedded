f6d4fa2d458e56f0db2eb27f7b23a6b764a87b9c
Alex Reisler <a.reisler@tu-braunschweig.de>
2024-04-24 02:44:42 +0200

    Files changes

---
+++ microcontroller/types.h
@@ -0,0 +1,23 @@
+
+/* types - Utility include that defines useful types
+ *
+ * Written in 2013 by Sven Hesse <drmccoy@drmccoy.de>.
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and
+ * neighboring rights to this software to the public domain worldwide.
+ * This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+#ifndef TYPES_H_
+#define TYPES_H_
+#include <stdint.h>
+typedef uint8_t bool_t;
+#ifndef FALSE
+	#define FALSE 0
+#endif
+#ifndef TRUE
+	#define TRUE 1
+#endif
+#endif /* TYPES_H_ */
